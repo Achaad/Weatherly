1295bf205537391d37a5413621bd23cca8e8bba9
Achaad <perant@zoho.com>
2018-09-15 17:41:12 +0300

    Added statistics, need to alter gradle.build

--- src/main/java/com/weatherly/demo/Database/Statistics.java
+++ src/main/java/com/weatherly/demo/Database/Statistics.java
@@ -1,7 +1,5 @@
 package com.weatherly.demo.Database;
 
-import sun.util.resources.Bundles;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
