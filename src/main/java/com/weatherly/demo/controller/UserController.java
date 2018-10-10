057b530cf19be0f21955ae8c1d7dbb796056d77b
Achaad <perant@zoho.com>
2018-10-10 15:30:11 +0300

    Created mail sending service, application properties are sent via private messages

--- src/main/java/com/weatherly/demo/controller/UserController.java
+++ src/main/java/com/weatherly/demo/controller/UserController.java
@@ -1,14 +1,43 @@
 package com.weatherly.demo.controller;
 
+import com.weatherly.demo.services.MailSender;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.text.DateFormat;
+import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.LinkedHashMap;
+import java.util.Locale;
 
 @Controller
 public class UserController {
 
   @GetMapping("/user")
-  public String homePage(Model model) {
+  public String homePage(Model model, OAuth2Authentication authentication, HttpServletRequest servletRequest) {
+
+
+    // Gets user information
+    LinkedHashMap<String, String> userDetails =
+        (LinkedHashMap<String, String>) authentication.getUserAuthentication().getDetails();
+    String userMail = userDetails.get("email");
+    String userName = userDetails.get("name");
+    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH);
+    String ipAddress = servletRequest.getHeader("x-real-ip"); //Returns IP Address
+    // this.ipAddress = "193.40.12.10"; // TODO: Revert back for deployment
+    String visitTime = dateFormat.format(Calendar.getInstance().getTime());
+    String subject = "Login";
+
+    MailSender mailSender = new MailSender();
+    MessageFormat format = new MessageFormat("Hello {0},\n" +
+        "We have noticed that You have accessed our webpage on {1} from this ip: {2}. If it was " +
+        "not You, please contact us at weatherly.me@gmail.com");
+    format.format(userName, visitTime, ipAddress);
+    mailSender.sendSimpleMessage(userMail, subject, format.toString());
 
     return "user";
   }
---
+++ src/main/java/com/weatherly/demo/services/MailSender.java
@@ -0,0 +1,25 @@
+package com.weatherly.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import javax.validation.constraints.Email;
+
+
+public class MailSender {
+
+  @Autowired
+  public JavaMailSender mailSender;
+
+  public void sendSimpleMessage(String to, String subject, String  text) {
+
+    SimpleMailMessage message = new SimpleMailMessage();
+    message.setTo(to);
+    message.setSubject(subject);
+    message.setText(text);
+    mailSender.send(message);
+  }
+}
