79a19486101cc64b9be518c9afc4363a8fb36607
Peavalu <train_troll@yahoo.com>
2018-10-12 09:14:21 +0300

    Added JS redirect functionality to search bar

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,7 +1,16 @@
 
 function getWeather(){
 
-    alert(document.getElementById("search").value)
+    var result = document.getElementById("search").value;
+
+    var parts = result.split(",");
+
+    alert(parts + " length: " + parts.length);
+
+    if(parts.length != 3)
+        alert("Sisend vales formaadis.")
+    else
+        window.location.href = "http://weatherly.me/data/weather?country=" + parts[0] + "&regionName=" + parts[1] + "&city=" + parts[2];
 
 }
 
