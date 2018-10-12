328b73be0cfabc66fc0b10333ddf028d487cb504
Peavalu <train_troll@yahoo.com>
2018-10-12 09:26:13 +0300

    Added weather landing page

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -10,7 +10,7 @@ function getWeather(){
     if(parts.length != 3)
         alert("Sisend vales formaadis.")
     else
-        window.location.href = "http://weatherly.me/data/weather?country=" + parts[0] + "&regionName=" + parts[1] + "&city=" + parts[2];
+        window.location.href = "http://localhost:8080/data/weather?country=" + parts[0] + "&regionName=" + parts[1] + "&city=" + parts[2];
 
 }
 
