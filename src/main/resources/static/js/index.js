dc196d5f7105688aa815b9dae836f63c8482022f
Peavalu <train_troll@yahoo.com>
2018-10-12 09:40:33 +0300

    Added issue functionalities

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -10,7 +10,7 @@ function getWeather(){
     if(parts.length != 3)
         searchBar.placeholder = "Vales formaadis sisend!"
     else
-        window.location.href = "http://localhost:8080/data/weather?country=" + parts[0] + "&regionName=" + parts[1] + "&city=" + parts[2];
+        window.location.href = "https://weatherly.me/data/weather?country=" + parts[0] + "&regionName=" + parts[1] + "&city=" + parts[2];
 
 }
 
