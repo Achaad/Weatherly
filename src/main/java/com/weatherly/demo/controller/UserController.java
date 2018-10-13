9b290d836fe7266bcd421ac1a27e292d5fb9d64e
Achaad <perant@zoho.com>
2018-10-13 14:31:32 +0300

    Small change

--- src/main/java/com/weatherly/demo/controller/UserController.java
+++ src/main/java/com/weatherly/demo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.weatherly.demo.controller;
 
 import com.weatherly.demo.services.MailSender;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
