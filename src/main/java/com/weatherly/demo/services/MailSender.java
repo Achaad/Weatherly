7884b694341e7fcdbd6adc83bb594890cc9bd7ca
Achaad <perant@zoho.com>
2018-10-13 14:34:32 +0300

    Small change

--- src/main/java/com/weatherly/demo/services/MailSender.java
+++ src/main/java/com/weatherly/demo/services/MailSender.java
@@ -4,12 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class MailSender {
 
   @Autowired
-  private JavaMailSender mailSender;
+  public JavaMailSender mailSender;
 
   public void sendSimpleMessage(String to, String subject, String  text) {
 
