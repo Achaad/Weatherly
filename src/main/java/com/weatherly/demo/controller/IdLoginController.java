3227fca71d1242c034ec045e8f0f73219c2822f9
Achaad <perant@zoho.com>
2018-11-04 18:22:53 +0200

    Created login page for idcard

---
+++ src/main/java/com/weatherly/demo/controller/IdLoginController.java
@@ -0,0 +1,15 @@
+package com.weatherly.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IdLoginController {
+
+    @GetMapping("/idlogin")
+    public String homePage(Model model){
+
+        return "idlogin";
+    }
+}
