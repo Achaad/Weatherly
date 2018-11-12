453cc5c87e843eb09d71c6bc5c510a09421da4d9
Achaad <perant@zoho.com>
2018-11-12 22:23:56 +0200

    Created an entity class for user table

---
+++ src/main/java/com/weatherly/demo/entities/User.java
@@ -0,0 +1,74 @@
+package com.weatherly.demo.entities;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * Entity class that represents User DB table
+ */
+@Entity
+@Table(name="user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String fullName;
+
+    private String mail;
+
+    @ElementCollection
+    private List<String> locations;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public List<String> getLocations() {
+        return locations;
+    }
+
+    public void setLocations(List<String> locations) {
+        this.locations = locations;
+    }
+}
