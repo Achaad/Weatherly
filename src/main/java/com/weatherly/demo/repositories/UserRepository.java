f176f4880efacb9508a080855388eb255856c023
Achaad <perant@zoho.com>
2018-11-12 22:41:44 +0200

    Created basic repository for User table

---
+++ src/main/java/com/weatherly/demo/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.weatherly.demo.repositories;
+
+import com.weatherly.demo.entities.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    @Query(value = "SELECT locations FROM user WHERE id = :userId", nativeQuery = true)
+    List<String> findLocationsById(@Param("userId") Integer userId);
+}
