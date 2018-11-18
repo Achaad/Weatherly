148158f3d0a55dd22825f6f9fb2c5fb405153fe9
Achaad <perant@zoho.com>
2018-11-18 10:09:28 +0200

    Webpage displays errors if the location was not submitted and info text if submission was successful

--- src/main/java/com/weatherly/demo/controller/UserController.java
+++ src/main/java/com/weatherly/demo/controller/UserController.java
@@ -3,10 +3,7 @@ package com.weatherly.demo.controller;
 import com.weatherly.demo.entities.User;
 import com.weatherly.demo.entities.UserLocation;
 import com.weatherly.demo.repositories.UserRepository;
-import com.weatherly.demo.services.MailSender;
-import com.weatherly.demo.services.MailSenderImpl;
-import com.weatherly.demo.services.UserService;
-import com.weatherly.demo.services.UserServiceImpl;
+import com.weatherly.demo.services.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -87,19 +84,50 @@ public class UserController {
   }
 
 
-
+  /**
+   * Saves location added by the user to the database
+   * @param location String. Should be in format Country,Region,City unless we implement a hashmap for locations
+   * @param redirectAttributes used to redirect back to the /user page
+   * @param authentication used to get the information about the logged in user
+   * @return redirects back to the user
+   */
   @PostMapping(value = "/user/submitLocation")
   public String uploadLocation(@RequestParam("userLocation") String location, RedirectAttributes redirectAttributes, OAuth2Authentication authentication) {
+    // Gets details of the user
     LinkedHashMap<String, String> userDetails =
             (LinkedHashMap<String, String>) authentication.getUserAuthentication().getDetails();
-    User user = userRepository.findByUserId(userDetails.get("id"));
-    user.addLoaction(new UserLocation(location));
+    User user = userRepository.findByUserId(userDetails.get("id")); // Gets user based on its userId
+
+    // We check if the string is in the right format (wether it consists of three elements separated by commas)
+    if (location.split(",").length != 3) {
+      redirectAttributes.addFlashAttribute("message", "Location is in wrong format.");
+      return "redirect:/user";
+    }
+
+    // Check if the location is existant and findable at yr.no
+    UserLocation userLocation = new UserLocation(location);
+    Location loc = new Location(userLocation.getCountry(), userLocation.getRegion(), userLocation.getCity());
+    try {
+      if (new Weather(loc).getWeatherState().equals("tundmatu")) {
+        redirectAttributes.addFlashAttribute("message", "Location is non-existant. Check it at https://yr.no");
+        return "redirect:/user";
+      }
+    } catch (NullPointerException e) {
+      redirectAttributes.addFlashAttribute("message", "Location is non-existant. Check it at https://yr.no");
+      return "redirect:/user";
+    }
+
+    // If it reaches here, then everything is correct
+    user.addLoaction(userLocation);
+
     User newUser = userRepository.save(user);
+    /* TODO: maybe remove, not sure if these check are necessary...
     if (user.equals(newUser)) {
       //return ResponseEntity.status(HttpStatus.OK).build();
     } else {
       //return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).build();
-    }
+    }*/
+    redirectAttributes.addFlashAttribute("message", "Location submitted successfully.");
     return "redirect:/user";
   }
 }
